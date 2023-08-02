8b09b47830f4e78c171a92cc738f4e5c5f94f5ba
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-08-02 15:17:31 -0400

    tokens and styles oh my

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css';
 import './index.css';
 import App from './App.tsx';
 
