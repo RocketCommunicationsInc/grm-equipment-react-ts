f528df04d93ddcdbf70e39142340a21c99e087a4
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-08-02 11:54:22 -0600

    add equipment pages

---
+++ src/Components/EquipmentTree/EquipmentTree.tsx
@@ -0,0 +1,8 @@
+import { RuxTree } from '@astrouxds/react';
+import './EquipmentTree.css';
+
+const EquipmentTree = () => {
+  return <RuxTree>Tree</RuxTree>;
+};
+
+export default EquipmentTree;
---
+++ src/Components/InoperableEquipment/InoperableEquipment.tsx
@@ -0,0 +1,7 @@
+import './InoperableEquipment.css';
+
+const InoperableEquipment = () => {
+  return <div>Inoperable</div>;
+};
+
+export default InoperableEquipment;
