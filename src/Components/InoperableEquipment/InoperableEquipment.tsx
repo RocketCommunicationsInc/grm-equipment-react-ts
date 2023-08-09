430fafe6cdda7447caf7fa2c7b26e68e0b6643b9
Maria Paterno <97068764+Paterma@users.noreply.github.com>
2023-08-09 10:05:16 -0600

    add real data to inoperable equipment

--- src/Components/InoperableEquipment/InoperableEquipment.tsx
+++ src/Components/InoperableEquipment/InoperableEquipment.tsx
@@ -1,8 +1,6 @@
-import { RuxContainer } from '@astrouxds/react';
+import { RuxContainer, RuxMonitoringIcon } from '@astrouxds/react';
 import './InoperableEquipment.css';
-import EquipmentIcons from '../EquipmentIcons/EqupimentIcons';
 import { useAppContext } from '../../providers/AppProvider';
-import { capitalize } from '../../utils';
 
 type PropTypes = {
   selectEquipment: () => void;
@@ -11,27 +9,92 @@ type PropTypes = {
 const InoperableEquipment = ({ selectEquipment }: PropTypes) => {
   const { state }: any = useAppContext();
 
+  const category = state.equipment.map((equip: string) => equip);
+
+  const digital = category
+    .filter((category: any) => category.category === 'digital')
+    .map((equip: any) => equip.equipmentString);
+
+  const facilities = category
+    .filter((category: any) => category.category === 'facilities')
+    .map((equip: any) => equip.equipmentString);
+
+  const comms = category
+    .filter((category: any) => category.category === 'comms')
+    .map((equip: any) => equip.equipmentString);
+
+  const RF = category
+    .filter((category: any) => category.category === 'rf')
+    .map((equip: any) => equip.equipmentString);
+
   return (
     <RuxContainer className='inoperable-equipment'>
       <div slot='header'>Inoperable Equipment</div>
-      {Object.keys(state.equipmentByCategory).map((category, index) => (
-        <RuxContainer className='section' key={`${category}${index}`}>
-          <span>
-            {category === 'rf' ? category.toUpperCase() : capitalize(category)}
-          </span>
-
-          {/* <div className='no-equipment'>No Inoperable Equipment</div> */}
-          {Object.keys(state.equipmentByCategory[category]).map(
-            (config, index) => (
-              <EquipmentIcons
-                key={`${config}${index}`}
-                equipmentString={config}
+      <RuxContainer className='section'>
+        <span>Digital</span>
+        {/* <div className='no-equipment'>No Inoperable Equipment</div> */}
+        <ul>
+          {digital.map((equip: string) => (
+            <li>
+              <RuxMonitoringIcon
+                key={equip}
+                status='normal'
+                icon='center-focus-weak'
+                label={equip}
                 onClick={selectEquipment}
               />
-            )
-          )}
-        </RuxContainer>
-      ))}
+            </li>
+          ))}
+        </ul>
+      </RuxContainer>
+      <RuxContainer className='section'>
+        <span>Facilities</span>
+        <ul>
+          {facilities.map((equip: string) => (
+            <li>
+              <RuxMonitoringIcon
+                key={equip}
+                status='normal'
+                icon='center-focus-weak'
+                label={equip}
+                onClick={selectEquipment}
+              />
+            </li>
+          ))}
+        </ul>
+      </RuxContainer>
+      <RuxContainer className='section'>
+        <span>Comms</span>
+        <ul>
+          {comms.map((equip: string) => (
+            <li>
+              <RuxMonitoringIcon
+                key={equip}
+                status='normal'
+                icon='center-focus-weak'
+                label={equip}
+                onClick={selectEquipment}
+              />
+            </li>
+          ))}
+        </ul>
+      </RuxContainer>
+      <RuxContainer className='section'>
+        <span>RF</span>
+        <ul>
+          {RF.map((equip: string) => (
+            <li>
+              <RuxMonitoringIcon
+                key={equip}
+                status='normal'
+                icon='center-focus-weak'
+                label={equip}
+                onClick={selectEquipment}
+              />
+            </li>
+          ))}
+        </ul>
+      </RuxContainer>
     </RuxContainer>
   );
 };
