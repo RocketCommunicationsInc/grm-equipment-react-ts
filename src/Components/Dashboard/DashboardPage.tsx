a381743ce0e174016114d36ea5ff61a129c62fed
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-08-16 15:04:29 -0400

    fix tabs, make inoperable icons clickable

--- src/Components/Dashboard/DashboardPage.tsx
+++ src/Components/Dashboard/DashboardPage.tsx
@@ -1,17 +1,47 @@
 import { useState } from 'react';
 import EquipmentDetailsPage from '../EquipmentDetailsPage/EquipmentDetailsPage';
 import EquipmentTree from '../EquipmentTree/EquipmentTree';
+import { Equipment } from '../../Types/Equipment';
+import { useAppContext } from '../../providers/AppProvider';
 import './DashboardPage.css';
 
 const Dashboard = () => {
   const [inoperablePanelShow, setInoperablePanelShow] = useState<boolean>(true);
+  const { state, dispatch }: any = useAppContext();
+
+  const handleSelectedEquipment = (equipment: Equipment) => {
+    // always set the selected equipment to the current equipment in app state.
+    dispatch({ type: 'CURRENT_EQUIPMENT', payload: equipment });
+
+    // hide the inoperable panel in favor of the equipment panel
+    setInoperablePanelShow(false);
+
+    // check if equipment already has an existing tab (if it is in selectedEquipment state)
+    const equipmentFound =
+      state.selectedEquipment &&
+      state.selectedEquipment.some((equipmentItem: Equipment) => {
+        if (equipmentItem.id === equipment.id) {
+          return true;
+        }
+        return false;
+      });
+
+    //if it has a tab, don't add it, return
+    if (equipmentFound) {
+      return;
+    } else {
+      // if it doesn't have a tab, add to selectedEquipment state array, which will then re-render component and create new tab.
+      dispatch({ type: 'ADD_SELECTED_EQUIPMENT', payload: equipment });
+    }
+  };
 
   return (
     <main className='dashboard'>
-      <EquipmentTree setInoperablePanelShow={setInoperablePanelShow} />
+      <EquipmentTree handleSelectedEquipment={handleSelectedEquipment} />
       <EquipmentDetailsPage
         inoperablePanelShow={inoperablePanelShow}
         setInoperablePanelShow={setInoperablePanelShow}
+        handleSelectedEquipment={handleSelectedEquipment}
       />
     </main>
   );
