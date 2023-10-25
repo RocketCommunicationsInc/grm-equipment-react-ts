bb6235c7b285288d732bf9cb23c6421b4857c8a9
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-10-25 13:05:59 -0400

    Tweak tree so it only shows available items

--- src/Components/EquipmentTree/EquipmentTree.tsx
+++ src/Components/EquipmentTree/EquipmentTree.tsx
@@ -20,7 +20,7 @@ const EquipmentTree = ({ equipmentList, handleSelected }: PropTypes) => {
   const treeNodeRef = useRef<Set<HTMLRuxTreeNodeElement>>(new Set());
   const tree = useRef<HTMLRuxTreeElement | null>(null);
   const configArray: string[] = ['A', 'B', 'C', 'D', 'E'];
-  const categoryArray: string[] = ['comms', 'digital', 'facilities', 'rf'];
+  const categoryArray: string[] = ['Comms', 'Digital', 'Facilities', 'RF'];
 
   const expandTreeNodeParent = (treeNode: HTMLRuxTreeNodeElement) => {
     const parentNode = treeNode?.parentElement?.closest('rux-tree-node');
@@ -80,42 +80,73 @@ const EquipmentTree = ({ equipmentList, handleSelected }: PropTypes) => {
     };
   }, [tree]);
 
+  const checkCategory = (category: string) => {
+    return (
+      equipmentList.filter(
+        (equip) => equip.category.toLowerCase() === category.toLowerCase()
+      ).length > 1
+    );
+  };
+
+  const checkConfig = (category: string, config: string) => {
+    return (
+      equipmentList.filter(
+        (equip) =>
+          equip.category.toLowerCase() === category.toLowerCase() &&
+          equip.config.toLowerCase() === config.toLowerCase()
+      ).length > 1
+    );
+  };
+
   return (
     <RuxContainer className='equipment-tree'>
       <RuxTree ref={tree}>
-        {categoryArray.map((category) => (
-          <RuxTreeNode key={category}>
-            {category === 'rf' ? category.toUpperCase() : capitalize(category)}
-            {configArray.map((config) => (
-              <RuxTreeNode slot='node' key={`${category}${config}`}>
-                Component {config}
-                {equipmentList.map(
-                  (equipment: any, index: number) =>
-                    equipment.category === category &&
-                    equipment.config === config && (
-                      <RuxTreeNode
-                        key={`${category}${config}${index}`}
-                        id={equipment.id}
-                        slot='node'
-                        onRuxtreenodeselected={() =>
-                          handleSelectedEquipment(equipment)
-                        }
-                        selected={equipment.id === selected}
-                        ref={(el) => {
-                          if (el) {
-                            treeNodeRef.current.add(el);
-                          }
-                        }}
-                      >
-                        <RuxStatus slot='prefix' status={equipment.status} />
-                        {equipment.config}-{equipment.equipmentString}
+        {categoryArray.map(
+          (category) =>
+            checkCategory(category) && (
+              <RuxTreeNode key={category}>
+                {category === 'rf'
+                  ? category.toUpperCase()
+                  : capitalize(category)}
+                {configArray.map(
+                  (config) =>
+                    checkConfig(category, config) && (
+                      <RuxTreeNode slot='node' key={`${category}${config}`}>
+                        Component {config}
+                        {equipmentList.map(
+                          (equipment: any, index: number) =>
+                            equipment.category.toLowerCase() ===
+                              category.toLowerCase() &&
+                            equipment.config.toLowerCase() ===
+                              config.toLowerCase() && (
+                              <RuxTreeNode
+                                key={`${category}${config}${index}`}
+                                id={equipment.id}
+                                slot='node'
+                                onRuxtreenodeselected={() =>
+                                  handleSelectedEquipment(equipment)
+                                }
+                                selected={equipment.id === selected}
+                                ref={(el) => {
+                                  if (el) {
+                                    treeNodeRef.current.add(el);
+                                  }
+                                }}
+                              >
+                                <RuxStatus
+                                  slot='prefix'
+                                  status={equipment.status}
+                                />
+                                {equipment.config}-{equipment.equipmentString}
+                              </RuxTreeNode>
+                            )
+                        )}
                       </RuxTreeNode>
                     )
                 )}
               </RuxTreeNode>
-            ))}
-          </RuxTreeNode>
-        ))}
+            )
+        )}
       </RuxTree>
     </RuxContainer>
   );
